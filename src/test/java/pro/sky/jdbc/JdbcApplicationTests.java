f3807ebbf7b66c09c3d91dd3956964c99f03495f
Polyxen1a <maslowa@inbox.ru>
2023-05-16 23:51:19 +0300

    Initial commit

---
+++ src/test/java/pro/sky/jdbc/JdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
